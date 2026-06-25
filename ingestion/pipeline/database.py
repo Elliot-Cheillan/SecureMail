7055fbc7e09dd04690d4addb906a764e979a7a71
Elliot Cheillan <elliot.cheillan27@gmail.com>
2026-06-25 15:00:39 +0200

    Some changes in the website UI and temporary add some features in the pipeline

--- ingestion/pipeline/database.py
+++ ingestion/pipeline/database.py
@@ -15,6 +15,7 @@ def _create_mails_table(cursor):
                         DKIM_Result TEXT,
                         Content TEXT,
                         Filename TEXT,
+                        Raw_EML TEXT,
                         Label TEXT)"""
     )
 
